904732fa57a10236ff6d0cf8ef621d416f885f73
Gabriela R Ramos <gabrielarramos@GabrielaRRamos.local>
2023-12-14 11:33:43 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+let palabras = document.getElementById(word-count)
+
+let caracteres = document.getElementById(character-count)
+conteocaracteres = caracteres.lenght;
+
+let caracteresnoespacio = document.getElementById(character-no-spaces-count)
+
+let contarnumeros = document.getElementById(number-count)
+
+let sumanumeros = document.getElementById(number-sum)
+
+let promedioespaciopalabras = document.getElementById(word-lenght-average)
+
+
+
+
+let resetearboton= document.getElementById(reset-button)
+resetearboton.addEventListener('click',function value()){
+
+}
+
